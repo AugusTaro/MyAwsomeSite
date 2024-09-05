21da518596298cdd4790ae1d78adc1e42406cd3f
eXplosion <yazuya-yazuya-0515@outlook.jp>
2024-09-05 22:31:44 +0900

    カテゴリーボタン修正ーカテゴリフィルタリング機能追加

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -1,15 +1,18 @@
+import { Link } from "gatsby"
 import React from "react"
 export const CategoryButtons = ({ category }) => {
   return (
     <>
       {category.map(category => {
         return (
-          <button
-            className="mx-0.5 btn btn-sm p-1 shadow-md font-sans"
-            key={category.id}
-          >
-            {category.name}
-          </button>
+          <Link to={`/blogIndex?category=${encodeURIComponent(category.name)}`}>
+            <div
+              className="mx-0.5 btn btn-sm p-1 shadow-md font-sans"
+              key={category.id}
+            >
+              {category.name}
+            </div>
+          </Link>
         )
       })}
     </>
--- src/pages/blogIndex.jsx
+++ src/pages/blogIndex.jsx
@@ -9,6 +9,18 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = `Title`
   const posts = data.allMicrocmsBlogs.nodes
 
+  // URLパラメータの取得
+  // URLSerchParamsは、クエリパラメータを解析するためのJS標準API
+  const searchParams = new URLSearchParams(location.search)
+  const categoryName = searchParams.get("category")
+
+  // フィルタリングロジック
+  const filteredPosts = categoryName
+    ? posts.filter(post =>
+        post.category.some(category => category.name === categoryName)
+      )
+    : posts
+
   return (
     <Layout location={location} title={siteTitle}>
       <Seo
@@ -17,26 +29,34 @@ const BlogIndex = ({ data, location }) => {
       />
       <div className="container mx-auto px-4 py-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts.map(post => {
-            const title = post.title || "Title"
-            const link = `/articles/${post.blogsId}`
-            const key = post.id
-            const createdAt = post.createdAt
-            const category = post.category
-            const eyecatch = post.eyecatch.url
-            return (
-              <div className="p-2">
-                <ArticleCard
-                  key={key}
-                  title={title}
-                  createdAt={createdAt}
-                  link={link}
-                  category={category}
-                  eyecatch={eyecatch}
-                />
-              </div>
-            )
-          })}
+          {filteredPosts.length === 0 ? (
+            <div className="text-center py-10">
+              <h2 className="text-2xl font-bold text-center">
+                コンテンツが存在しません
+              </h2>
+            </div>
+          ) : (
+            filteredPosts.map(post => {
+              const title = post.title || "Title"
+              const link = `/articles/${post.blogsId}`
+              const key = post.id
+              const createdAt = post.createdAt
+              const category = post.category
+              const eyecatch = post.eyecatch.url
+              return (
+                <div className="p-2">
+                  <ArticleCard
+                    key={key}
+                    title={title}
+                    createdAt={createdAt}
+                    link={link}
+                    category={category}
+                    eyecatch={eyecatch}
+                  />
+                </div>
+              )
+            })
+          )}
         </div>
       </div>
     </Layout>
